90032c1cf00e0f31f393528dc25b8950a8c72181
Mary Ballantyne <mballantyne777@gmail.com>
2022-09-24 12:12:32 -0600

    add working form to create a new bulk discount then reflected on bulk discounts index page

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -10,4 +10,11 @@ class BulkDiscountsController < ApplicationController
   def new
     @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    @merchant.bulk_discounts.create!(percent_discount: params[:percent_discount], qty_threshold: params[:qty_threshold])
+
+    redirect_to "/merchant/#{@merchant.id}/bulk_discounts"
+  end
 end
